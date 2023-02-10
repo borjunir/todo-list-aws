3ab9042b14bc7f94318bc696c6597a2ecbc5512e
BorjUnir <borja.rodriguezlopezd077@comunidadunir.net>
2023-02-10 10:42:10 +0000

    Correciones por flake8

--- src/translate.py
+++ src/translate.py
@@ -4,18 +4,17 @@ import todoList
 
 
 def translate(event, context):
-        # process text
         result = todoList.get_item(event['pathParameters']['id'])
         translate = todoList.get_translate(
                 result['text'],
                 event['pathParameters']['lang'])
-        
+      
         if result and translate:
             result['text'] = translate
             response = {
                 "statusCode": 200,
                 "body": json.dumps(result,
-                                    cls=decimalencoder.DecimalEncoder)
+                                   cls=decimalencoder.DecimalEncoder)
                 }
         else:
             response = {
